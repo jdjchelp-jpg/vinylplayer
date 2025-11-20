9d17a04b2dbdf27ba8cdd501e9e31c335818a021
jdjchelp-jpg <jdjchelp@gmail.com>
2025-11-20 06:43:14 -0500

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,33 @@
+import { VinylPlayer } from './services/VinylPlayer.js';
+
+export const sendAnalytics = (data = '') => {
+    fetch(`backend/analytics.php?data=${encodeURIComponent(data)}`)
+        .then(response => response.text())
+        .then(data => {
+            //console.log('Analytics sent:', data);
+        })
+        .catch(error => {
+            console.error('Analytics error:', error);
+        });
+};
+
+// Отправляем базовую аналитику при загрузке
+sendAnalytics();
+
+// Инициализируем плеер
+document.addEventListener("DOMContentLoaded", () => new VinylPlayer());
+
+function displayLocalTime() {
+    const now = new Date();
+    const timeString = now.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
+    document.getElementById('clock').innerText = timeString;
+}
+
+// Update the time every second
+setInterval(displayLocalTime, 1000);
+
+// Run the function once when the page loads
+displayLocalTime();
+
+
+
